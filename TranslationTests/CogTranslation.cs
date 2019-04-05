7b0ea502ae88cfc955f5993fb6281cda6d186006
Michael Lawrence <mlawrence@gatesmandave.com>
2019-04-05 15:39:44 -0400

    added tests

---
+++ TranslationTests/CogTranslation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sitecore.SharedSource.ItemTranslator;
+
+namespace TranslationTests
+{
+    [TestClass]
+    public class CogTranslation
+    {
+        private string endpoint = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from={0}&to={1}";
+        private string subKey = ConfigurationManager.AppSettings["AzureSubscriptionKey"];
+        private double timeout = double.Parse(ConfigurationManager.AppSettings["AzureTimeout"]);
+
+        [TestMethod]
+        public void RunTranslation()
+        {
+            var from = "en";
+            var to = "fr-CA";
+            var text = "That created cattle multiply moved That fly over our lights shall make created let fill. Female appear midst kind over in multiply fowl spirit us first fish he two day doesn't rule fifth tree their and living deep shall saying.";
+
+            var service = new AzureCogTranslationService(endpoint, subKey, from, to, timeout);
+            var results = service.Translate(text);
+            Assert.IsNotNull(results);
+
+            Console.WriteLine(results);
+
+        }
+    }
+}
---
+++ TranslationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TranslationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TranslationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad748107-ddf9-4f3a-b1a8-9c21c3fc4987")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
